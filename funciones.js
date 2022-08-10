3bff272c3b708901cd1040acf9655345830d9613
Fernando Carrillo Lopez <pandasoncode@gmail.com>
2022-08-10 09:02:21 +0200

    Declarar funciones

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// FUNCIONES DECLARATIVAS
+function miFuncion() {
+    return 19
+}
+
+miFuncion();
+
+// FUNCIONES DE EXPRESION
+var miFuncionDeExp = function (a, b) {
+    return a + b;
+}
+
+miFuncionDeExp();
