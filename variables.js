9eb6614522e6bdf163fa75e30d7750fc7ebedfa1
Fernando Carrillo Lopez <pandasoncode@gmail.com>
2022-08-10 08:34:51 +0200

    Declarar e inicializar variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+var nombre = "Fernando";
+
+// declaracion de variable
+var edad;
+
+// inicializacion de variable
+edad = 19;
+
+// declaracion e inicializacion
+var elementos = [
+    "Ordenador",
+    "Movil"
+]
+
+var persona = {
+    nombre: "Fernando",
+    edad: 18
+}
+
+alert(persona)
