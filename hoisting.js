161f49f9fc97b8b4a39b7db7c3ef894ab9689bab
Fernando Carrillo Lopez <pandasoncode@gmail.com>
2022-08-10 10:19:27 +0200

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+// COMO miVariable NO EXISTE TODAVIA AL LEER ESTA LINEA
+// LO QUE HARA JAVASCRIPT AL LEERLO ES DECLARAR LA VARIABLE
+// var miVariable = undefined
+
+console.log(miVariable);
+var miVariable = "Fernando"
+
+saludar()
+
+function saludar() {
+    console.log("HOLA");
+}
+
+// EN EL CASO DE LAS FUNCIONES DECLARATIVAS 
+// ESTAS PUEDEN SER LLAMADAS ANTES DE SER DECLARADAS
