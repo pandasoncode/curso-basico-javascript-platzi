f75340c70038e93c4cb542028b8cc4261896ccea
Fernando Nicolas Carrillo Lopez <you@example.com>
2023-03-25 11:11:22 +0100

    Operadores

---
+++ operadores.js
@@ -0,0 +1,24 @@
+// BINARIOS
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log("Fernando " + "Carrillo");
+
+// UNARIOS
+console.log(true, !true);
+
+// COMPARACION
+console.log("3" == 3);
+console.log("3" === 3);
+console.log(3 < 3);
+console.log(3 <= 3);
+console.log(3 > 3);
+console.log(3 >= 3);
+
+// LOGICOS
+console.log(true && true);
+console.log(true && false);
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
