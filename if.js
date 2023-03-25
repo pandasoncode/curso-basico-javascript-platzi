891508b282d80bc03d15f5045b557e3ebafd985c
Fernando Nicolas Carrillo Lopez <you@example.com>
2023-03-25 11:23:55 +0100

    if

---
+++ if.js
@@ -0,0 +1,10 @@
+const edad = 18;
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
+
+// if ternario
+const mensaje = edad >= 18 ? "Eres mayor de edad" :  "Eres menor de edad";
+console.log(mensaje);
