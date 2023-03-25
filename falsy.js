f04c6e10b7f01d3c9724dd7ef915f9b73405393d
Fernando Nicolas Carrillo Lopez <you@example.com>
2023-03-25 10:52:45 +0100

    Valores truthy y falsy

---
+++ falsy.js
@@ -0,0 +1,9 @@
+// FALSY
+console.log("FALSY");
+console.log(Boolean());
+console.log(Boolean(0), "0");
+console.log(Boolean(null), "null");
+console.log(Boolean(NaN), "NaN");
+console.log(Boolean(undefined), "undefined");
+console.log(Boolean(false), "false");
+console.log(Boolean(""), `""`);
---
+++ truthy.js
@@ -0,0 +1,8 @@
+// TRUTHY
+console.log("TRUTHY");
+console.log(Boolean(" "), `" "`);
+console.log(Boolean(1), "1", "(CUALQUIER NUMERO DISTINTO DE 0)");
+console.log(Boolean([]), "[]", "(UN ARRAY VACIO)");
+console.log(Boolean({}), "{}", "(UN OBJETO VACIO)");
+console.log(Boolean(function(){}), "function(){}");
+console.log(Boolean(true), "true");
