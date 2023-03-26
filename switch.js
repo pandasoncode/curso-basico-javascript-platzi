e0f8186f929df7179b4db965861b7dfa62023755
Fernando Nicolas Carrillo Lopez <pandasoncode@gmail.com>
2023-03-26 13:16:46 +0200

    Switch

---
+++ switch.js
@@ -0,0 +1,13 @@
+const edad = 17;
+
+switch (edad >= 18) {
+    case true:
+        console.log("Eres mayor de edad");
+        break;
+    case false:
+        console.log("Menor de edad");
+        break;
+    default:
+        console.log("¿Como has llegado hasta aqui?");
+        break;
+}
